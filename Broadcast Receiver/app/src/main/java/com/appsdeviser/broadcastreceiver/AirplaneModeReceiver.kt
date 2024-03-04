44f71cde2c5ae7ff2b074e3fa7d08e90f6aa2b3d
Yuvraj Patel <yuvipatel003@gmail.com>
2024-03-03 20:23:37 -0500

    Register BroadcastReceiver for AirplaneMode change

---
+++ Broadcast Receiver/app/src/main/java/com/appsdeviser/broadcastreceiver/AirplaneModeReceiver.kt
@@ -0,0 +1,20 @@
+package com.appsdeviser.broadcastreceiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.provider.Settings
+
+class AirplaneModeReceiver: BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if(intent?.action == Intent.ACTION_AIRPLANE_MODE_CHANGED) {
+            val isTurnedOn = Settings.Global.getInt(
+                context?.contentResolver,
+                Settings.Global.AIRPLANE_MODE_ON
+            ) != 0
+
+            println("Is airplane mode enabled ? $isTurnedOn")
+        }
+    }
+}
--- Broadcast Receiver/app/src/main/java/com/appsdeviser/broadcastreceiver/MainActivity.kt
+++ Broadcast Receiver/app/src/main/java/com/appsdeviser/broadcastreceiver/MainActivity.kt
@@ -1,46 +1,42 @@
 package com.appsdeviser.broadcastreceiver
 
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.appsdeviser.broadcastreceiver.ui.theme.BroadcastReceiverTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val airplaneModeReceiver = AirplaneModeReceiver()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        registerReceiver(
+            airplaneModeReceiver,
+            IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+            )
+
         setContent {
             BroadcastReceiverTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BroadcastReceiverTheme {
-        Greeting("Android")
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(airplaneModeReceiver)
     }
 }
