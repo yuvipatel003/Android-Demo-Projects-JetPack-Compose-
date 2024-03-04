38edac0e7eacf30649aa0cc3e84f292050a4c5e2
Yuvraj Patel <yuvipatel003@gmail.com>
2024-03-03 19:44:33 -0500

    Initial Commit - Broadcast Receiver

---
+++ Broadcast Receiver/app/src/main/java/com/appsdeviser/broadcastreceiver/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.appsdeviser.broadcastreceiver
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.appsdeviser.broadcastreceiver.ui.theme.BroadcastReceiverTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BroadcastReceiverTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BroadcastReceiverTheme {
+        Greeting("Android")
+    }
+}
